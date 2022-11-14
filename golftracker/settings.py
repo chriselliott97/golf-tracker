bfb8e2e4613c9a8a544e3d4116b84deecbf15131
chriselliott97 <chris.elliott6497@gmail.com>
2022-11-14 10:33:20 -0500

    config heroku

--- golftracker/settings.py
+++ golftracker/settings.py
@@ -1,7 +1,7 @@
 import environ
 env = environ.Env()
 environ.Env.read_env()
-SECRET_KEY = env('-300tu1d1ad!8pxmqm3q85vv&(p5g8li&e+jv=b_)=cy-rd=i=')
+
 
 DEBUG = env('DEBUG') == 'True' 
 
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2sfz1a*s%1cweej7mw2@z!)6crzk1j*$b-m=+&i6x&ul=_*f$j'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
